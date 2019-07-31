2b6287263831c8b5596313f37f07222f86fbd052
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-31 02:18:16 +0200

    changed the environmental variable for heroku deployment

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,7 @@
-const knex = require("knex");
+const knex = require('knex');
 
-const config = require("../knexfile.js");
+const knexConfig = require('../knexfile.js');
 
-module.exports = knex(config.development);
+const dbEnv = process.env.DB_ENV || 'development';
+
+module.exports = knex(knexConfig[dbEnv]);
