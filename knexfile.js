ae07d7d1149221ba8e0137bb7e2bd34738b5c840
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-31 02:33:14 +0200

    changed the db enviroment for production

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,32 @@
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: "./data/splitthebill.db3"
+      filename: './data/splitthebill.db3',
     },
     migrations: {
-      directory: "./data/migrations"
+      directory: './data/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: './data/seeds',
     },
     // needed to use foreign keys
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
-  }
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
